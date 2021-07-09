76ae19a2775083cd727b3582d90c600ed4c4483c
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2021-07-09 18:21:23 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+include("header.php");
+?>
+
+    <div class="row" style="margin-top:30px;">
+      <div class="small-12">
+
+        <p>Wanna get in touch. Email us at <a href="mailto:support@buyEasy.com">support@buyEasy.com</a></p>
+
+       
+      </div>
+    </div>
+
+<?php
+include("footer.php");
+
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+    include("header.php");
+?>
+<div style="text-align:center;margin: 0px 85px;"> 
+    <h1 style="color:green"><b>buyEasy</b></h1>
+    <h3 style="color:grey"><i>Buy organically grown vegetables directly from the farmers that grow them!</i></h3>
+    <br>
+    <img src="./images/pic1.jpeg" height="10" />
+</div>
+
+<?php
+    include("footer.php");
+?>
